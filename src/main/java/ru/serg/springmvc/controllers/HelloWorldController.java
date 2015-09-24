c9e0a4ef67eeec0e421f53b24103593c7c6689ce
sergcloud <sergcloud@gmail.com>
2015-09-25 02:55:34 +0300

    It's first commit. It was created Hello World page using Spring MVC, Maven, Jetty etc

---
+++ src/main/java/ru/serg/springmvc/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package ru.serg.springmvc.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloWorldController implements Controller {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return new ModelAndView("helloWorld");
+    }
+}
