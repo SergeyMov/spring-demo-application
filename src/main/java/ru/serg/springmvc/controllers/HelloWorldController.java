0d5598c1261cdabe2dc1289ddd45b98b8bfdd811
sergcloud <sergcloud@gmail.com>
2015-09-25 13:46:23 +0300

    Corrected some errors (used annotation Controller, etc)

--- src/main/java/ru/serg/springmvc/controllers/HelloWorldController.java
+++ src/main/java/ru/serg/springmvc/controllers/HelloWorldController.java
@@ -3,10 +3,13 @@ package ru.serg.springmvc.controllers;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-public class HelloWorldController implements Controller {
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/")
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         return new ModelAndView("helloWorld");
     }
--- src/main/webapp/WEB-INF/views/helloWorld.jsp
+++ src/main/webapp/WEB-INF/views/helloWorld.jsp
@@ -1,12 +1,11 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-	<h3>Здравствуй, мир!</h3>
-</body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Заголовок страницы Здравствуй, мир!</title>
+	</head>
+	<body>
+		<h3>Здравствуй, мир!</h3>
+	</body>
 </html>
